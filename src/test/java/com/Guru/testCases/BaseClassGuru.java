4517e8a880b3b16daf67ea2fe899a3b92c5cc8fa
testdummy-qa <testdummy.qa@gmail.com>
2020-06-08 03:50:13 +0530

    Base Class updated to arrange for desired browser capabilities in project

--- src/test/java/com/Guru/testCases/BaseClassGuru.java
+++ src/test/java/com/Guru/testCases/BaseClassGuru.java
@@ -1,11 +1,14 @@
-package com.Guru.testCases;
+  package com.Guru.testCases;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import com.Guru.utilities.ReadConfig;
 
@@ -18,14 +21,34 @@ public class BaseClassGuru {
 	public static WebDriver driver; 
 	public static Logger log;
 	
+	@Parameters("browser")
+	
 	@BeforeClass
-	public void setup()
+	public void setup(String brwsr)
 	{
-	System.setProperty("webdriver.chrome.driver",readconfig.getChromePath());
-	driver = new ChromeDriver();
 	
 	log=Logger.getLogger("AutomationPractice");
 	PropertyConfigurator.configure("log4j.properties");
+	
+	if(brwsr.equals("chrome"))
+	{
+		System.setProperty("webdriver.chrome.driver", readconfig.getChromePath());
+		driver = new ChromeDriver();
+	}
+	else if(brwsr.equals("firefox"))
+	{
+		System.setProperty("webdriver.gecko.driver", readconfig.getFireFoxPath());
+		driver= new FirefoxDriver();
+	}
+	else if (brwsr.equals("ie"))
+	{
+		System.setProperty("webdriver.ie.driver", readconfig.getIePath());
+		driver = new InternetExplorerDriver();
+	}
+	
+	driver.get(baseURL);        	
+	log.info("URL is opened");   // for logging 
+	
 	}
 	
 	@AfterClass
