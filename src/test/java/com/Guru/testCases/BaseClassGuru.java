c6b4eee1ac6ba1ead1ea7c5efa359220e70ddc45
testdummy-qa <testdummy.qa@gmail.com>
2020-06-18 01:45:58 +0530

    BaseClass > Adding user defined method to take screenshots at test failures - to support reporting

--- src/test/java/com/Guru/testCases/BaseClassGuru.java
+++ src/test/java/com/Guru/testCases/BaseClassGuru.java
@@ -1,7 +1,13 @@
   package com.Guru.testCases;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -57,6 +63,15 @@ public class BaseClassGuru {
 		driver.quit();
 	}
 	
+// user defined method to be called at the test failures	
+	public void captureScreen(WebDriver driver, String tname) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File target = new File(System.getProperty("user.dir")+"/Screenshots/"+tname+".png");
+		FileUtils.copyFile(source, target);
+		System.out.println("Screenshot taken");
+		
+	}
 	
 	
 }
